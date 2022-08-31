250ec9b6e92edf129fefb4fa1dda1ce2d25acaa1
Jaehyeok <milkboy2564@naver.com>
2022-08-31 15:28:13 +0900

    FIX: auth provider 삭제

--- src/App.js
+++ src/App.js
@@ -4,22 +4,21 @@ import AuthPage from './pages/Auth';
 import TodoPage from './pages/Todo';
 import PublicRoutes from './utils/routes/PublicRoutes';
 import PrivateRoutes from './utils/routes/PrivateRoutes';
+
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <GlobalStyle />
-        <Routes>
-          <Route element={<PublicRoutes />}>
-            <Route index element={<AuthPage />} />
-          </Route>
-          <Route element={<PrivateRoutes />}>
-            <Route path="todo" element={<TodoPage />} />
-          </Route>
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route element={<PublicRoutes />}>
+          <Route index element={<AuthPage />} />
+        </Route>
+        <Route element={<PrivateRoutes />}>
+          <Route path="todo" element={<TodoPage />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
